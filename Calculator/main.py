9267030129cee38c4f09794e3b1029f4c7203701
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2023-03-11 12:46:28 +0000

    calculator finished.

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,41 +1,47 @@
 
-# import art
-# print(art.logo)
+from art import logo
 
-#Adding
-def add (n1, n2):
-    return n1 + n2
+def add(n1, n2):
+  return n1 + n2
 
-#Substract
-def sub (n1, n2):
-    return n1 - n2
+def subtract(n1, n2):
+  return n1 - n2
 
-#Multiply
-def mult (n1, n2):
-    return n1 * n2
+def multiply(n1, n2):
+  return n1 * n2
 
-#Division
-def div (n1, n2):
-    if n2 == 0:
+def divide(n1, n2):
+  if n2 == 0:
         return "syntax error"
-    return n1 / n2
+  return n1 / n2
 
-#Hashmap operations
 operations = {
-    "+" : add,
-    "-" : sub,
-    "*" : mult,
-    "/" : div, 
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
 }
 
-num1 = int(input("First number:"))
-num2 = int(input("First number:"))
+def calculator():
+  print(logo)
 
-for symbol in operations:
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
     print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-operation_symbol = input("Pick an operation from the line above:")
-calculation_function = operations[operation_symbol] #utilizamos el input de simbolos, y lo pasamos como llave para sacar el value
-answer = calculation_function(num1, num2)
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      clear()
+      calculator()
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+calculator()
