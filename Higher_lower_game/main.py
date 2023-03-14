9c1132967718290d6048441475b465f51de500b3
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2023-03-14 23:30:18 +0000

    Higher or lower Game

---
+++ Higher_lower_game/main.py
@@ -0,0 +1,12 @@
+from draw import logo, vs
+from game_data import data
+import random
+
+print(logo)
+
+print(f"Compare A: #constructor de data")
+
+print(vs)
+
+print(f"Against B: #constructor de data")
+answer = input("Who has more followers? Type 'A' or 'B': ").title()
