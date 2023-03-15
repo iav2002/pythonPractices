756c6c9941b505b637584f9dcf471886875c66c0
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2023-03-15 17:08:01 +0000

    higher game finished

--- MiniPracticas/grades.py
+++ MiniPracticas/grades.py
@@ -18,6 +18,4 @@ for student in student_scores:
         student_grades[student] = "Acceptable"
     else: 
          student_grades[student] = "Fail"
-
-# 🚨 Don't change the code below 👇
 print(student_grades)
