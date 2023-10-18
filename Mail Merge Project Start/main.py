a10a7ea89017b6dde51cd7718e63fd09b880cde3
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2023-10-18 15:22:01 +0100

    Automatation of invitations by Email using Python

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -6,3 +6,21 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+PLACEHOLDER = "[name]"
+
+
+with open("Mail Merge Project Start/Input/Names/invited_names.txt") as starts:
+    base = starts.readlines() #separamos los nombres del archivo y los ponemos en un array
+  
+
+with open("Mail Merge Project Start/Input/Letters/starting_letter.txt") as letter_file:
+    letter_content = letter_file.read() 
+    print(letter_content)
+    for name in base:
+        clean_name = name.strip()
+        final = letter_content.replace(PLACEHOLDER, clean_name)
+        with open (f"Mail Merge Project Start/Output/ReadyToSend/letter_for_{final}", mode =  "w") as completed_letter:
+            completed_letter.write(final)
+
+        
--- file_system/basics.py
+++ file_system/basics.py
@@ -1,6 +1,9 @@
-with open("my_file.txt") as file:
+with open("my_file.txt") as file: #Reading our file 
     contents = file.read()
     print(contents)
 
-with open("my_file.txt", mode = "w") as file:
+with open("my_file.txt", mode = "w") as file: #Writing to our file
+    file.write("\nNew text.") 
+
+with open("my_file.txt", mode = "a") as file: #Appeding strings to our file
     file.write("\nNew text.") 
