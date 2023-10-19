f232925619ec9dc0ffb0d0ed9a0a18d4b04421d0
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2023-10-19 23:44:34 +0100

    Beggining of learning Pandas

---
+++ MiniPracticas/file_system/basics.py
@@ -0,0 +1,9 @@
+with open("/Users/ignacioalarconvarela/VsCode_Python/MiniPracticas/file_system/my_file.txt") as file: #Reading our file 
+    contents = file.read()
+    print(contents)
+
+with open("/Users/ignacioalarconvarela/VsCode_Python/MiniPracticas/file_system/my_file.txt", mode = "w") as file: #Writing to our file
+    file.write("\nNew text.") 
+
+with open("/Users/ignacioalarconvarela/VsCode_Python/MiniPracticas/file_system/my_file.txt", mode = "a") as file: #Appeding strings to our file
+    file.write("\nNew text.") 
--- file_system/basics.py
+++
@@ -1,9 +0,0 @@
-with open("my_file.txt") as file: #Reading our file 
-    contents = file.read()
-    print(contents)
-
-with open("my_file.txt", mode = "w") as file: #Writing to our file
-    file.write("\nNew text.") 
-
-with open("my_file.txt", mode = "a") as file: #Appeding strings to our file
-    file.write("\nNew text.") 
