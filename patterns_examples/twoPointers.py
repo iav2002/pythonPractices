1d875a6ff73266a6a770149ccbeec5bc4bbffcda
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2023-11-17 23:49:03 +0000

    Two Pointers solutions

---
+++ patterns_examples/twoPointers.py
@@ -0,0 +1,40 @@
+def check_for_target(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        # curr is the current sum
+        if nums[left] + nums[right] == target:
+            return True
+        elif nums[left] + nums[right]> target:
+            right -= 1
+        elif nums[left] + nums[right]:
+            left += 1
+    
+    return False
+arr = [1,2,3,4,5,6]
+
+print(check_for_target(arr, 100))
+
+## Combining two sorted arrays in O(n) instead of using 0(n log N)
+def combine(arr1, arr2):
+    # ans is the answer
+    ans = []
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            ans.append(arr1[i])
+            i += 1
+        else:
+            ans.append(arr2[j])
+            j += 1
+    
+    while i < len(arr1):
+        ans.append(arr1[i])
+        i += 1
+    
+    while j < len(arr2):
+        ans.append(arr2[j])
+        j += 1
+    
+    return ans
