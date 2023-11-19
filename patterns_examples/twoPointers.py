f9a67b9b08ec92b8d08f6bd01d8307420cbd0c30
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2023-11-19 01:16:48 +0000

    Sliding window fix

--- patterns_examples/twoPointers.py
+++ patterns_examples/twoPointers.py
@@ -1,40 +1,51 @@
-def check_for_target(nums, target):
-    left = 0
-    right = len(nums) - 1
+# def check_for_target(nums, target):
+#     left = 0
+#     right = len(nums) - 1
 
-    while left < right:
-        # curr is the current sum
-        if nums[left] + nums[right] == target:
-            return True
-        elif nums[left] + nums[right]> target:
-            right -= 1
-        elif nums[left] + nums[right]:
-            left += 1
+#     while left < right:
+#         # curr is the current sum
+#         if nums[left] + nums[right] == target:
+#             return True
+#         elif nums[left] + nums[right]> target:
+#             right -= 1
+#         elif nums[left] + nums[right]:
+#             left += 1
     
-    return False
-arr = [1,2,3,4,5,6]
+#     return False
 
-print(check_for_target(arr, 100))
 
-## Combining two sorted arrays in O(n) instead of using 0(n log N)
-def combine(arr1, arr2):
-    # ans is the answer
+# ## Combining two sorted arrays in O(n) instead of using 0(n log N)
+# def combine(arr1, arr2):
+#     # ans is the answer
+#     ans = []
+#     i = j = 0
+#     while i < len(arr1) and j < len(arr2):
+#         if arr1[i] < arr2[j]:
+#             ans.append(arr1[i])
+#             i += 1
+#         else:
+#             ans.append(arr2[j])
+#             j += 1
+    
+#     while i < len(arr1):
+#         ans.append(arr1[i])
+#         i += 1
+    
+#     while j < len(arr2):
+#         ans.append(arr2[j])
+#         j += 1
+    
+#     return ans
+
+def sortedSquares(nums: list[int]):
+    numero = nums[0]
     ans = []
-    i = j = 0
-    while i < len(arr1) and j < len(arr2):
-        if arr1[i] < arr2[j]:
-            ans.append(arr1[i])
-            i += 1
-        else:
-            ans.append(arr2[j])
-            j += 1
-    
-    while i < len(arr1):
-        ans.append(arr1[i])
-        i += 1
-    
-    while j < len(arr2):
-        ans.append(arr2[j])
-        j += 1
-    
+    for i in nums:
+        square =  numero * numero
+        ans.append(square)
+
     return ans
+
+nums = [2,2,3,4,5,6]
+
+print(sortedSquares(nums))
