770f6df1ec0a05d2e168a1f0207452a93624800e
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2024-06-04 23:58:19 +0100

    summer

--- Csv_Panda/main.py
+++ Csv_Panda/main.py
@@ -1,9 +1,15 @@
 import csv
 
-with open("/Users/ignacioalarconvarela/VsCode_Python/Csv_Panda/weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+# with open("/Users/ignacioalarconvarela/VsCode_Python/Csv_Panda/weather_data.csv") as data_file:
+    # data = csv.reader(data_file)
+    # temperatures = []
+    # for row in data:
+    #     if row[1] != "temp":
+    #         temperatures.append(int(row[1]))
+    # print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("/Users/ignacioalarconvarela/VsCode_Python/Csv_Panda/weather_data.csv")
+    
+print(data["temp"])
