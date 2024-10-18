1653b41bfb4e5970b0dc843e93ddcd7ff443c486
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2024-10-18 09:10:08 +0100

    Day 26

--- countyCounter/main.py
+++ countyCounter/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,9 +7,37 @@ image = "countyCounter/ireland-blank-map.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-def get_mouse_click_coor(x,y):
-    print(x, y)
+data =  pd.read_csv("countyCounter/32_counties.csv")
 
-turtle.onscreenclick(get_mouse_click_coor)
+county_list = data["county"].to_list()
+#print(state_list)
+guessed_county = []
+missed_county = []
 
-turtle.mainloop()
+while len(guessed_county) < 34:
+    answer_county = screen.textinput(title = f"{len(guessed_county)} out of 32",
+                                     prompt="Guess the County").title()
+    print(answer_county)
+
+    if answer_county == "Exit":
+
+        for county in county_list:
+            if county not in guessed_county:
+                missed_county.append(county)
+            
+        print(missed_county)
+        new_data = pd.DataFrame(missed_county)
+        counter = len(missed_county)
+        new_data.to_csv(f"/Users/ignacioalarconvarela/Developer/VsCode_Python/countyCounter/{counter}_counties_to_learn.csv")
+        break
+
+    if answer_county in county_list:
+
+        guessed_county.append(answer_county)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        county_data = data[data.county == answer_county]
+        t.goto(float(county_data.x.item()),float(county_data.y.item()))
+        t.write(answer_county)
+
--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -7,26 +7,38 @@ image = "/Users/ignacioalarconvarela/Developer/VsCode_Python/day-25-us-states-ga
 screen.addshape(image)
 turtle.shape(image)
 
-data =pd.read_csv("day-25-us-states-game-start/50_states.csv")
+data = pd.read_csv("day-25-us-states-game-start/50_states.csv")
 state_list = data["state"].to_list()
 
 guessed_states = []
+missing_states = []
 
 while len(guessed_states) < 50:
 
     answer_state = screen.textinput(title = f"{len(guessed_states)} States out of 50",
                                      prompt= "What's another state's name:").title()
     print(answer_state)
+    
+    if answer_state == "Exit":
+       
+        for state in state_list:
+            if state not in guessed_states:
+                missing_states.append(state)          
+        
+        print(missing_states)        
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("day-25-us-states-game-start/states_to_learn.csv")
+        break  
 
     if answer_state in state_list:
-
         guessed_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
         state_data = data[data.state == answer_state]
-        t.goto(state_data.x.item(), state_data.y.item())
+        t.goto(state_data.x.item(),state_data.y.item())
         t.write(answer_state)
 
 
-screen.exitonclick()
+        
+    #states_to_learn.csv 
