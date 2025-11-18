3db90439efddc7671a54aabfbac6508284ad6017
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2025-11-18 14:34:51 +0000

    Buggs Fixed

--- countyCounter/main.py
+++ countyCounter/main.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 screen = turtle.Screen()
 screen.title("Ireland Counties")
-image = "countyCounter/ireland-blank-map.gif"
+image = "ireland-blank-map.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-data =  pd.read_csv("countyCounter/32_counties.csv")
+data =  pd.read_csv("32_counties.csv")
 
 county_list = data["county"].to_list()
 #print(state_list)
@@ -26,7 +26,7 @@ while len(guessed_county) < 34:
                 missed_county.append(county)
             
         print(missed_county)
-        new_data = pd.DataFrame(missed_county)
+        new_data = pd.DataFrame(missed_county) 
         counter = len(missed_county)
         new_data.to_csv(f"/Users/ignacioalarconvarela/Developer/VsCode_Python/countyCounter/{counter}_counties_to_learn.csv")
         break
@@ -41,3 +41,5 @@ while len(guessed_county) < 34:
         t.goto(float(county_data.x.item()),float(county_data.y.item()))
         t.write(answer_county)
 
+
+##if it doesnt run check the image file
---
+++ day-27/main.py
@@ -0,0 +1,15 @@
+import tkinter
+
+window =tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width = 500, height =300)
+
+my_label = tkinter.Label(text = "I Am a Label")
+
+
+
+
+
+
+
+window.mainloop()
